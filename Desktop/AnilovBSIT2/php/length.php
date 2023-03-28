4fc78a7d95a812a49c6b7d851a3805bd6fb11422
Anilov_Carlos <carlos.anilovbriant@auf.edu.ph>
2023-03-28 19:27:21 +0800

    First Commit

---
+++ Desktop/AnilovBSIT2/php/length.php
@@ -0,0 +1,11 @@
+<?php
+function compute($value){
+    return $value *=2;
+}
+
+
+$str = strlen("twister");
+echo compute($str);
+
+
+?>
---
+++ Desktop/AnilovBSIT2/php/odds.php
@@ -0,0 +1,15 @@
+<?php
+
+
+function countodd()
+{
+    for ($j=1; $j<=99; $j+=2)
+    {
+        echo $j."\n";
+    }
+   
+}
+
+
+countodd();
+?>
